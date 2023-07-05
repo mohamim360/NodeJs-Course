17beb9bb92fd2f4e4fd54502aa9e219dd70c2c9e
mohamim360 <mohamim360@gmail.com>
2023-07-05 13:03:01 +0600

    Routing requests

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+
+  if (url === "/") {
+    
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">SEND</button></input></form></body>');
+    res.write("</html>");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
@@ -9,10 +18,12 @@ const server = http.createServer((req, res) => {
   res.write("<body><h1>Hello from my Node.js server</h1></body>");
   res.write("</html>");
   res.end();
-
 });
 
 server.listen(3000);
 
+//if (url === "/") { ... }: This condition checks if the requested URL is the root ("/") of the server.
 
-//res.setHeader("Content-Type", "text/html");, it is telling the client that the response body will contain HTML content. This allows the client to interpret and render the received content appropriately. Web browsers, for example, use this information to determine how to display the HTML content to the user.
+//Inside the if block, the server sends an HTML response back to the client. It writes the HTML content using the res.write() method and ends the response with res.end(). In this case, the HTML form is displayed, allowing the user to enter a message and submit it.
+
+//If the requested URL is not the root ("/"), the server sends a default HTML response. It writes a basic HTML page with a heading saying "Hello from my Node.js server".
