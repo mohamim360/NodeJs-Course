f92137672436cc5ec7f4d5789111047ef73ecd17
mohamim360 <mohamim360@gmail.com>
2023-07-05 09:39:18 +0600

    sending responses and creating a html page through the response

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -1,10 +1,18 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url , req.method , req.headers);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
 
 });
 
-
-
 server.listen(3000);
+
+
+//res.setHeader("Content-Type", "text/html");, it is telling the client that the response body will contain HTML content. This allows the client to interpret and render the received content appropriately. Web browsers, for example, use this information to determine how to display the HTML content to the user.
