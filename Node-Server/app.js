8571898fc21bf38f8b65336d36ddb2960bf8e730
mohamim360 <mohamim360@gmail.com>
2023-07-05 09:20:37 +0600

    understanding requests

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url , req.method , req.headers);
+
 });
 
-//Listen now actually starts a process where nodejs will not immediately exit our script but where nodejs will instead keep this running to listen
+
 
 server.listen(3000);
