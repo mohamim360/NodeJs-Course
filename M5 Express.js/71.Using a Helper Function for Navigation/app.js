f1b5779424a9ac8053a3e51b5da194b16f9b83a0
mohamim360 <mohamim360@gmail.com>
2023-08-01 18:57:14 +0600

    71.Using a Helper Function for Navigation..edit 70

---
+++ M5 Express.js/71.Using a Helper Function for Navigation/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+
+const path = require('path');
+
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+   res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})//catch all middleware
+
+app.listen(3000);
--- M5 Express.js/71.Using a Helper Function for Navigation/routes/admin.js
+++ M5 Express.js/71.Using a Helper Function for Navigation/routes/admin.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require('../utilities/path')
+
 const path = require('path');
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+  res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
-//  you can use .. instant of ../
 
-//  /admin/add-product =>Post
 router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
