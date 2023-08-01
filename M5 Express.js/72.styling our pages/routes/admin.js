d74bbf7b53b0e494c9fb7685918bfb2013d05008
mohamim360 <mohamim360@gmail.com>
2023-08-01 19:59:08 +0600

    72.styling our pages

---
+++ M5 Express.js/72.styling our pages/routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+const rootDir = require('../utilities/path')
+
+const path = require('path');
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir,'views','add-product.html'));
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
