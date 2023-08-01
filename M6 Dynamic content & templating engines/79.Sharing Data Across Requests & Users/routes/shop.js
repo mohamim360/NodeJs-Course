2a129deb0576d3a338cd8fe8bcd712c470366ceb
mohamim360 <mohamim360@gmail.com>
2023-08-02 00:08:08 +0600

    79.Sharing Data Across Requests & Users

---
+++ M6 Dynamic content & templating engines/79.Sharing Data Across Requests & Users/routes/shop.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const path = require('path');
+
+const rootDir = require('../utilities/path')
+
+const adminData = require('./admin')
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  console.log('shop.js',adminData.products)
+  res.sendFile(path.join(rootDir,'views','shop.html'));
+});
+
+
+module.exports = router;
+
