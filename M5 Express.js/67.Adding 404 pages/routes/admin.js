9f51841f639ffef0eb0f2a2c5b89c025eeef2391
mohamim360 <mohamim360@gmail.com>
2023-08-01 10:41:42 +0600

    68.Filtering paths..edit 66,67

--- M5 Express.js/67.Adding 404 pages/routes/admin.js
+++ M5 Express.js/67.Adding 404 pages/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/cart" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
   );
---
+++ M5 Express.js/68.Filtering paths/routes/admin.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+//we can repeat the path here because we got different methods, get and post, so these will be two different routes too
+
+//  /admin/add-product =>GET
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
+  );
+});
+
+//  /admin/add-product =>Post
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
