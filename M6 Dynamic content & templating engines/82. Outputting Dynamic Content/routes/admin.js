f9b0625fa503321e9eec4fec9006ca1ea09d0513
mohamim360 <mohamim360@gmail.com>
2023-08-02 17:36:08 +0600

    84.Converting HTML Files to Pug

--- M6 Dynamic content & templating engines/82. Outputting Dynamic Content/routes/admin.js
+++ M6 Dynamic content & templating engines/82. Outputting Dynamic Content/routes/admin.js
@@ -2,21 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-const rootDir = require('../utilities/path')
+const rootDir = require("../utilities/path");
 
-const path = require('path');
+const path = require("path");
 
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir,'views','add-product.html'));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 router.post("/add-product", (req, res) => {
-  products.push({title : req.body.title})
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
 exports.routes = router;
 exports.products = products;
-
