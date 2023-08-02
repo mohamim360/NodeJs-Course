dcfd4c41b68746dc31aa87337f0cf81fd8b8511e
mohamim360 <mohamim360@gmail.com>
2023-08-02 12:08:34 +0600

    81.Installing & Implementing Pug

---
+++ M6 Dynamic content & templating engines/81.Installing & Implementing Pug/routes/admin.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const rootDir = require('../utilities/path')
+
+const path = require('path');
+
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir,'views','add-product.html'));
+});
+
+router.post("/add-product", (req, res) => {
+  products.push({title : req.body.title})
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
+
