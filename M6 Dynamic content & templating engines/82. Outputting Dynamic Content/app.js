3b6d3d557f4e3f9f98cfaae1713226e974877c33
mohamim360 <mohamim360@gmail.com>
2023-08-02 14:12:30 +0600

    82. Outputting Dynamic Content

---
+++ M6 Dynamic content & templating engines/82. Outputting Dynamic Content/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const adminData = require("./routes/admin");
+
+const path = require("path");
+
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
