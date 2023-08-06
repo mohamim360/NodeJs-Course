1a354b24448e39e58ae707c420af514e3b86e982
mohamim360 <mohamim360@gmail.com>
2023-08-07 00:23:27 +0600

    91+92.Working on the Layout with Partials

---
+++ M6 Dynamic content & templating engines/91.working with EJS/routes/shop.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const path = require("path");
+
+const rootDir = require("../utilities/path");
+
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+
+  res.render("shop", { prods: products, pageTitle: "shop" ,path : "/"});
+});
+
+module.exports = router;
