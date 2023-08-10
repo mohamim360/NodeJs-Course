1e6cd4a701b37bef8c06e6d9bece01f90c281327
mohamim360 <mohamim360@gmail.com>
2023-08-11 03:59:03 +0600

    102 have error

--- M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
+++ M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
@@ -42,3 +42,7 @@ module.exports = class Product {
     return products;
   }
 };
+
+/*Asynchronous Nature of fs.readFile: The fs.readFile function is asynchronous, which means it doesn't block the execution of the program. In your current code, you're returning products outside of the callback function passed to fs.readFile, which means it will be executed before the file reading operation is complete. As a result, the function will return products before the data is actually read from the file, and you'll likely get an empty array.
+
+Missing Callback: Since fs.readFile is asynchronous, you need to provide a callback function to handle the result of the file reading operation. Your code currently doesn't have a mechanism to pass the read data back to the caller once the file is read.*/ 
