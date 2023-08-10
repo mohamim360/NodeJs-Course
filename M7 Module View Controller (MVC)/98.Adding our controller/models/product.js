3aec1f913aafb654236e8a02010e18313be47530
mohamim360 <mohamim360@gmail.com>
2023-08-10 23:45:40 +0600

    101. Storing Data in Files Via the Model

--- M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
+++ M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
@@ -1,14 +1,44 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
-	constructor(t){
-		this.title = t;
-	}
+  constructor(t) {
+    this.title = t;
+  }
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
 
-	save(){
-		products.push(this);
-	}
-	static fetchAll(){
-		return products;
-	}
-}
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.watchFile(p, JSON.stringify(products),(err)=>{
+		console.log(err);
+	  });
+    });
+  }
+
+  
+  static fetchAll() {
+
+	const p = path.join(
+		path.dirname(require.main.filename),
+		"data",
+		"products.json"
+	  );
+
+	fs.readFile(p, (err,fileContent) => {
+      if(err){
+		return [];
+	  }
+	  return JSON.parse(fileContent);
+	})
+    return products;
+  }
+};
