b52dfc592b9360cbac0838d6b317df6c8db6840b
mohamim360 <mohamim360@gmail.com>
2023-08-11 23:30:51 +0600

    103. Refactoring the File Storage Code..

--- M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
+++ M7 Module View Controller (MVC)/98.Adding our controller/models/product.js
@@ -1,41 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductFromFile = (cb) => {
+ 
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    else{
+    cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
-  save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
 
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+  save() {
+   getProductFromFile(products =>{
+    products.push(this);
+    fs.writeFile(p, JSON.stringify(products), (err) => {
+      console.log(err);
     });
+   })
+
+   
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductFromFile(cb);
   }
 };
+
+/*Asynchronous Behavior: Due to the asynchronous nature of fs.readFile, the return statements within the callback won't have any effect on the outer function. The outer function will complete execution before the callback is even executed, and as a result, the return values won't be captured or used.*/
