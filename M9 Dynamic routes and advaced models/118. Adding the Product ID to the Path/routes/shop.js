5c9f74c1775c5fa71b82bf9dffdc5800f1a348e3
mohamim360 <mohamim360@gmail.com>
2023-08-15 23:59:02 +0600

    119

--- M9 Dynamic routes and advaced models/118. Adding the Product ID to the Path/routes/shop.js
+++ M9 Dynamic routes and advaced models/118. Adding the Product ID to the Path/routes/shop.js
@@ -10,8 +10,6 @@ router.get("/products",shopController.addProducts);
 
 router.get("/products/:productId",shopController.getProduct);
 
-//router.get("/products/delete");  --X not right
-/*delete would basically be treated as the dynamic segment.So if you had a dynamic segment and a specific route, you would have to put the more specific route first*/
 
 router.get("/cart",shopController.getCart);
 
