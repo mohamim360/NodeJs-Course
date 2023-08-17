8f6fd8cd8e67cbdc6d97943d6db02f9bacfe5378
mohamim360 <mohamim360@gmail.com>
2023-08-17 19:49:59 +0600

    125.Pre-Populating the Edit Product Page with Data

--- M9 Dynamic routes and advaced models/118. Adding the Product ID to the Path/controllers/admin.js
+++ M9 Dynamic routes and advaced models/118. Adding the Product ID to the Path/controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProducts = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false
   });
 };
 
@@ -19,13 +20,20 @@ exports.postAddProducts = (req, res) => {
 
 exports.getEditProducts = (req, res, next) => {
   const editMode = req.query.edit;
-  if(!editMode){
-    return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode,
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
