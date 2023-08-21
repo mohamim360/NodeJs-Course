03cf48d48381e618177b1417c81f4008391e918b
mohamim360 <mohamim360@gmail.com>
2023-08-21 07:12:24 +0600

    152. Defining a Model

--- M11 understanding sequelize/151. Connecting to the Database/models/product.js
+++ M11 understanding sequelize/151. Connecting to the Database/models/product.js
@@ -1,31 +1,27 @@
-const Cart = require("../models/cart");
-const db = require("../utilities/database");
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Sequelize = require("sequelize");
 
-  //save
+const sequelize = require("../utilities/database");
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title,price,imageUrl,description) VALUES (?,?,?,?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  //delete
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+const Product = sequelize.define("product", {
+  id: {
+    type: sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+ 
+module.exports = Product;
