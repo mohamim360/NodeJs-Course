577bf968b91b10e001752edcd46ea08f36d5e50e
mohamim360 <mohamim360@gmail.com>
2023-08-21 13:14:09 +0600

    154. Inserting Data & Creating a Product

--- M11 understanding sequelize/151. Connecting to the Database/controllers/admin.js
+++ M11 understanding sequelize/151. Connecting to the Database/controllers/admin.js
@@ -13,15 +13,18 @@ exports.postAddProducts = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() =>{
-      res.redirect("/");
-      
+
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => {
+     // console.log(result);
+     console.log('data');
     })
     .catch((err) => console.log(err));
-  
 };
 
 exports.getEditProducts = (req, res, next) => {
