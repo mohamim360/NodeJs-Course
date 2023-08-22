e8d80993a3589e8a1a0a3a797c3b5b873e9dae84
mohamim360 <mohamim360@gmail.com>
2023-08-22 09:37:45 +0600

    169. Adding Existing Products & Retrieving Cart Items

--- M11 understanding sequelize/151. Connecting to the Database/controllers/shop.js
+++ M11 understanding sequelize/151. Connecting to the Database/controllers/shop.js
@@ -59,6 +59,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchCard;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -70,17 +71,18 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
+
       if (product) {
-        //....
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchCard.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchCard.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect("/cart");
