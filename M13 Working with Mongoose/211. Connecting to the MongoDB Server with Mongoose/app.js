1c5abc6bc15f4b2024fd6ef7fa4bc6cab5271d22
mohamim360 <mohamim360@gmail.com>
2023-09-03 15:57:34 +0600

    218. Adding and Using a User Model

--- M13 Working with Mongoose/211. Connecting to the MongoDB Server with Mongoose/app.js
+++ M13 Working with Mongoose/211. Connecting to the MongoDB Server with Mongoose/app.js
@@ -26,14 +26,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// app.use((req, res, next) => {
-//   User.findById("64e6b3b97be7daf4ae3722cc")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("64f4582b1d1e8ce9b35451e4")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -44,6 +44,18 @@ mongoose
     "mongodb+srv://mohamim360:M51lUp9tDsG545SF@cluster0.k4fqzxy.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then(user =>{
+      if(!user){
+        const user = new User({
+          name: 'Hamim',
+          email: 'Hamim@gmail.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save();
+      }
+    })
     console.log('connected');
     app.listen(3000);
   })
