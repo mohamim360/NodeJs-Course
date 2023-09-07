d9cbe82b7562319c90e60c21ca67e81f967aabe8
mohamim360 <mohamim360@gmail.com>
2023-09-07 21:37:18 +0600

    235. Setting a Cookie

--- M14 Sessions & cookies/234. Adding the Request Driven Login Solution/app.js
+++ M14 Sessions & cookies/234. Adding the Request Driven Login Solution/app.js
@@ -36,7 +36,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    "mongodb+srv://mohamim360:M51lUp9tDsG545SF@cluster0.k4fqzxy.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then(result => {
     User.findOne().then(user => {
--- M14 Sessions & cookies/234. Adding the Request Driven Login Solution/controllers/auth.js
+++ M14 Sessions & cookies/234. Adding the Request Driven Login Solution/controllers/auth.js
@@ -1,6 +1,16 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login'
+
+   const isLoggedIn = req.get("Cookie").split("=")[1];
+
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
+  
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
